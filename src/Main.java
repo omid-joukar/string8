219dfeeff1ae36cbe066c0c857d8d7352e018793
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-24 17:46:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by KPS on 7/24/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+            String str1 = "PHP Exercises and Python Exercises";
+            String str2 = "and";
+            System.out.println("Original String: " + str1);
+            System.out.println("Specified sequence of char values: " + str2);
+            System.out.println(str1.contains(str2));
+        }
+    
+}
